ccbb8b2cb8f630303c72cab7939abe6ec365bc84
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-03-20 14:57:15 +0600

    solution for day 6 is modified

--- day 6/solution.sql
+++ day 6/solution.sql
@@ -1,4 +1,4 @@
-SELECT c.name, o.amount
+SELECT c.state, SUM(o.amount) AS total_sales
 FROM customers c
 JOIN orders o ON c.id = o.customer_id
-WHERE c.state = 'CA' AND o.amount > 100;
+GROUP BY c.state;
